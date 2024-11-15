173492f2151f0fd45fc1bf804f29d246522084a3
Sébastien Besson <sbesson@glencoesoftware.com>
2024-11-15 10:08:42 +0000

    Update src/omero_cli_render.py
    Co-authored-by: pwalczysko <p.walczysko@dundee.ac.uk>

--- src/omero_cli_render.py
+++ src/omero_cli_render.py
@@ -122,7 +122,7 @@ SET_HELP = """Set rendering settings
 
     # Omitted fields will keep their current values.
     # Omitted channels will not be disabled unless --disable is used.
-    # All specified settings are applied to the given channel independently of
+    # All specified settings are applied to the given channel, whether or not 
     # active is set and its value.
     # If min and max have not been set on the channel (no StatsInfo on the
     # channel) then you must set both. If max and min already set, each can
